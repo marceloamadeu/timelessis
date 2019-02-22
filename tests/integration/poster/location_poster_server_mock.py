f028c80cabe81268d6cc255ced157a76067c8bbe
marceloamadeu <marceloandradeamadeu@gmail.com>
2019-02-22 18:23:05 -0300

    #366 Continuation

---
+++ tests/integration/poster/location_poster_server_mock.py
@@ -0,0 +1,5 @@
+
+
+from tests.integration.poster.poster_server_mock import PosterServerMock
+
+class LocationPosterServerMock(PosterServerMock):
---
+++ tests/integration/poster/location_poster_server_mock_test.py
@@ -0,0 +1,5 @@
+
+from tests.integration.poster.location_poster_server_mock
+    import LocationPosterServerMock
+
+class LocationTestPosterServerMock():
