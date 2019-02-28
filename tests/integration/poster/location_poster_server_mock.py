451785ab9ae705a7a80b3d444b91bd41d36b477e
marceloamadeu <marceloandradeamadeu@gmail.com>
2019-02-28 17:51:00 -0300

    #366 Corrections

--- tests/integration/poster/location_poster_server_mock.py
+++ tests/integration/poster/location_poster_server_mock.py
@@ -2,9 +2,9 @@
 from http.server import HTTPStatus
 from tests.integration.poster.poster_server_mock import PosterServerMock
 
-class LocationPosterServerMock():
+class LocationPosterServerMock(PosterServerMock):
 
-    def __init__(self):
+    def poster(self):
         port = free_port()
         server = PosterServerMock
         server.start(server)
